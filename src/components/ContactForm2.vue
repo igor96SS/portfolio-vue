a0355060b8b4663a0ef53a03691db12bb1ff2cb8
igor96SS <igorsimoes_96@hotmail.com>
2025-04-29 17:21:47 +0100

    testing form

---
+++ src/components/ContactForm2.vue
@@ -0,0 +1,14 @@
+<template>
+    <form name="contact" method="POST" netlify>
+      <input type="hidden" name="form-name" value="contact" />
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+      </div>
+      <button type="submit">Send</button>
+    </form>
+  </template>
+  
+  <script setup>
+  // No JavaScript here
+  </script>
