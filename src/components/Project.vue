04e3bacef11470c4414521549f4ef8eb0c711796
igor96SS <igorsimoes_96@hotmail.com>
2025-05-06 00:00:18 +0100

    retrieve data

---
+++ src/components/Project.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import SchoolProjects from '@/assets/data/school_projects.json'
+import PersonalProjects from '@/assets/data/personal_projects.json'
+import WorkProjects from '@/assets/data/work_projects.json'
+import { computed } from 'vue';
+
+const route = useRoute();
+const projectID = route.params.id;
+const projects = [SchoolProjects, PersonalProjects, WorkProjects].flat();
+
+const project = computed(()=> projects.find(p => p.id === projectID));
+
+</script>
+
+<template>
+<h1>{{ project.title }}</h1>
+</template>
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Project from '@/components/Project.vue';
+
+
+</script>
+
+<template>
+    <Project/>
+</template>
