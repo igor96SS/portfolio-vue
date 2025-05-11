6dfd04c8bda1662ce9dd0f863613014d862683d4
igor96SS <igorsimoes_96@hotmail.com>
2025-05-11 21:48:26 +0100

    change code to use proper structure with views

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,67 +1,49 @@
 <script setup>
-import { useRoute } from 'vue-router';
-import SchoolProjects from '@/assets/data/school_projects.json'
-import PersonalProjects from '@/assets/data/personal_projects.json'
-import WorkProjects from '@/assets/data/work_projects.json'
-import { computed } from 'vue';
-import BackButton from './BackButton.vue';
 
-const route = useRoute();
-const projectID = route.params.id;
-const projects = [SchoolProjects, PersonalProjects, WorkProjects].flat();
-
-const project = computed(()=> projects.find(p => p.id === projectID));
+defineProps({
+  project: Object
+});
 
 </script>
 
 <template>
-    <BackButton/>
-    <section class="bg-blue-500 min-h-screen p-6">
-        <div class="container m-auto py-10 px-6">
-            <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                <main>
-            <div
-              class="bg-gray-900 p-6 rounded-lg shadow-md text-center md:text-left"
-            >
-            <div
-                class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-              >
-                <i
-                  class="pi pi-map-marker text-lg text-orange-700 mr-2"
-                ></i>
-                <p class="text-orange-700">{{project.course + " - " + project.year}}</p>
-              </div>
+  
+  <section class="bg-blue-500 min-h-screen p-6">
+    <div class="container mx-auto max-w-5xl py-10 px-10">
+        
+        <div class="bg-gray-900 p-6 rounded-lg shadow-md text-center md:text-left">
+          <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+            <p class="text-orange-700">{{project.course + " - " + project.year}}</p>
+          </div>
 
-
-              <h1 class="text-3xl text-white font-bold mb-4">{{project.title}}</h1>
-              
-            </div>
-
-            <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-              <h3 class="text-green-800 text-lg font-bold mb-6">
-                Project Description
-              </h3>
-
-              <p class="mb-4">
-                {{project.description}}
-              </p>
-
-              <h3 class="text-green-800 text-lg font-bold mb-2">Technologies</h3>
-
-              <div class="flex flex-wrap gap-2 mt-2">
-                <span
-                  v-for="tech in project.technologies"
-                  :key="tech"
-                  class="px-3 py-1 bg-gray-100 text-gray-800 rounded-full text-sm font-medium"
-                >
-                  {{ tech }}
-                </span>
-
-              </div>
-            </div>
-          </main>
-            </div>
+          <h1 class="text-3xl text-white font-bold mb-4">{{project.title}}</h1>
+          
         </div>
-    </section>
+
+        <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+          <h3 class="text-green-800 text-lg font-bold mb-6">
+            Project Description
+          </h3>
+
+          <p class="mb-4">
+            {{project.description}}
+          </p>
+
+          <h3 class="text-green-800 text-lg font-bold mb-2">Technologies</h3>
+
+          <div class="flex flex-wrap gap-2 mt-2">
+            <span
+              v-for="tech in project.technologies"
+              :key="tech"
+              class="px-3 py-1 bg-gray-100 text-gray-800 rounded-full text-sm font-medium"
+            >
+              {{ tech }}
+            </span>
+
+          </div>
+        </div>
+
+    </div>
+  </section>
 
 </template>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,9 +1,22 @@
 <script setup>
+import { useRoute } from 'vue-router';
 import Project from '@/components/Project.vue';
+import SchoolProjects from '@/assets/data/school_projects.json'
+import PersonalProjects from '@/assets/data/personal_projects.json'
+import WorkProjects from '@/assets/data/work_projects.json'
+import { computed } from 'vue';
+import BackButton from '@/components/BackButton.vue';
 
+
+const route = useRoute();
+const projectID = route.params.id;
+const projects = [SchoolProjects, PersonalProjects, WorkProjects].flat();
+
+const project = computed(()=> projects.find(p => p.id === projectID));
 
 </script>
 
 <template>
-    <Project/>
+    <BackButton/>
+    <Project :project="project"/>
 </template>
