688a74e0add240e95fc04d2cd944364cfadc5329
igor96SS <igorsimoes_96@hotmail.com>
2025-05-12 22:31:54 +0100

    design changes

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -12,16 +12,13 @@ defineProps({
     <div class="container mx-auto max-w-5xl px-12">
         
         <div class="bg-gray-900 p-6 rounded-lg shadow-md text-center md:text-left">
-          <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-            <p class="text-orange-600">{{project.course + " - " + project.year}}</p>
-          </div>
-
-          <h1 class="text-3xl text-white font-bold mb-4">{{project.title}}</h1>
-          
+            
+          <h1 class="text-3xl text-white font-bold">{{project.title}}</h1>
+          <p class="text-gray-400 italic text-sm">{{project.course + " - " + project.year}}</p>
         </div>
 
         <div class="bg-white p-6 rounded-lg shadow-md ">
-          <h3 class="text-yellow-600 text-lg font-bold mb-4">
+          <h3 class="text-amber-600 text-lg font-bold mb-4">
             Project Description
           </h3>
 
@@ -29,13 +26,13 @@ defineProps({
             {{project.description}}
           </p>
 
-          <h3 class="text-yellow-600 text-lg font-bold mb-2">Technologies</h3>
+          <h3 class="text-amber-600 text-lg font-bold mb-2">Technologies</h3>
 
           <div class="flex flex-wrap gap-2 mt-2">
             <span
               v-for="tech in project.technologies"
               :key="tech"
-              class="px-3 py-1 bg-blue-200 text-gray-800 hover:bg-blue-400 hover:text-white rounded-full text-sm font-medium"
+              class="px-3 py-1 bg-sky-200 text-gray-800 hover:bg-sky-400 hover:text-white rounded-full text-sm font-medium"
             >
               {{ tech }}
             </span>
